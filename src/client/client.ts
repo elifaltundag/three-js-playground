6dc4d87e3e3068e2ee9a3a6a3516ecb49c8f9eef
elifaltundag <elif.altundag.x@gmail.com>
2023-07-04 16:17:40 +0300

    compare perspective and orthographic cameras with wireframed cube. add stylesheet

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,21 +2,30 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('hsl(0, 0%, 50%)')
+scene.background = new THREE.Color('hsl(0, 0%, 10%)')
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
+// Setup the frustum for the perspective camera
+const fov: number = 100; // field of view: degrees in vertical
+const aspect: number = (window.innerWidth / 2.1) / window.innerHeight; // width of canvas / height of canvar
+const near: number = 0.1; // start of camera's range
+const far: number = 1000;  // end of camera's range
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const cameraPerspective = new THREE.PerspectiveCamera(fov, aspect, near, far)
+cameraPerspective.position.z = 2
 
-new OrbitControls(camera, renderer.domElement)
+const canvasPerspective = document.getElementById("canvasPerspective") as HTMLCanvasElement
+const rendererPerspective = new THREE.WebGL1Renderer({canvas: canvasPerspective})
+rendererPerspective.setSize(window.innerWidth / 2.1 , window.innerHeight)
+
+
+const cameraOrthographic = new THREE.OrthographicCamera(-2,2,2,-2)
+cameraOrthographic.position.z = 2
+
+const canvasOrthographic = document.getElementById("canvasOrthographic") as HTMLCanvasElement
+const rendererOrthographic = new THREE.WebGL1Renderer({canvas: canvasOrthographic})
+rendererOrthographic.setSize(window.innerWidth / 2.1 , window.innerHeight)
+
+/* new OrbitControls(cameraPerspective, rendererPerspective.domElement) */
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
@@ -27,13 +36,13 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-window.addEventListener('resize', onWindowResize, false)
+/* window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    cameraPerspective.aspect = aspect
+    cameraPerspective.updateProjectionMatrix()
+    rendererPerspective.setSize(window.innerWidth / 2.1 , window.innerHeight)
     render()
-}
+} */
 
 function animate() {
     requestAnimationFrame(animate)
@@ -45,7 +54,8 @@ function animate() {
 }
 
 function render() {
-    renderer.render(scene, camera)
+    rendererPerspective.render(scene, cameraPerspective)
+    rendererOrthographic.render(scene, cameraOrthographic)
 }
 
 animate()
